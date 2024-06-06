8fde1455855428f2cd3476dc55028f5b2c46f0a0
BrianTahiel <briang.2012@hotmail.com>
2024-06-06 11:19:34 -0300

    adding column crud

--- src/models/board.tsx
+++ src/models/board.tsx
@@ -1,18 +1,17 @@
 import mongoose, { Schema, Document } from 'mongoose';
-
 interface IBoard extends Document {
     title: string;
     description: string;
     tasks: mongoose.Types.ObjectId[];
+    priorities: mongoose.Types.ObjectId[];
     createdBy: string;
 }
-
 const BoardSchema: Schema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
+    priorities: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Priority' }],
     createdBy: { type: String, required: true }
 }, { timestamps: true });
 
 export default mongoose.models.Board || mongoose.model<IBoard>('Board', BoardSchema);
-
